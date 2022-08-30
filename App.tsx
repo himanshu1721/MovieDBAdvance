345968b6aa122d61fbfce6fa7fda5154631bfade
Himanshu Sharma <himanshu.s@simformsolutions.com>
2022-08-30 14:38:03 +0530

    android splash screen and icon

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistStore} from 'redux-persist';
-import {store, persistor} from './app/features/store';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './app/features/store';
 import MainStack from './app/navigation';
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- android/app/src/main/java/com/moviedbadvance/MainActivity.java
+++ android/app/src/main/java/com/moviedbadvance/MainActivity.java
@@ -3,6 +3,8 @@ package com.moviedbadvance;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen; // add this
+import android.os.Bundle; // add this
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override                                             // add this
+  protected void onCreate(Bundle savedInstanceState) {  // add this
+    SplashScreen.show(this);                            // add this
+    super.onCreate(savedInstanceState);                 // add this
+  }
+
   @Override
   protected String getMainComponentName() {
     return "MovieDBAdvance";
